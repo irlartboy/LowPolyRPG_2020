da9a594bfe421e60061a5b92a9adf80119ffd312
irlartboy <ollie_king99@outlook.com>
2020-03-19 19:40:01 +1100

    first push

---
+++ Assets/Scripts/MouseLook_Camera.cs
@@ -0,0 +1,54 @@
+﻿
+using UnityEngine;
+
+public class MouseLook_Camera : MonoBehaviour
+{
+    public RotationalAxis axis = RotationalAxis.MouseX;
+
+    [Range(0, 20)]
+    public float sensitivityX = 15;
+    [Range(0, 20)]
+    public float sensitivityY = 15;
+
+    public float minY = -60;
+    public float maxY = 60;
+
+    float rotationY = 0;
+    void Start()
+    {
+        if (this.GetComponent<Rigidbody>())
+        {
+            this.GetComponent<Rigidbody>().freezeRotation = true;
+        }
+    }
+    void Update()
+    {
+
+        if (axis == RotationalAxis.MouseXandY)
+        {
+            float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
+            rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+            rotationY = Mathf.Clamp(rotationY, minY, maxY);
+            transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
+        }
+
+        else if (axis == RotationalAxis.MouseX)
+        {
+            transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityX, 0);
+        }
+
+        else
+        {
+            rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+            rotationY = Mathf.Clamp(rotationY, minY, maxY);
+            transform.localEulerAngles = new Vector3(-rotationY, transform.localEulerAngles.y, 0);
+        }
+
+    }
+}
+public enum RotationalAxis
+{
+    MouseXandY = 0,
+    MouseX,
+    MouseY
+}
---
+++ Assets/Scripts/Movement_Player.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class Movement_Player : MonoBehaviour
+{
+    public Vector3 moveDirection;
+
+    private CharacterController playerController;
+
+    public float jumpSpeed = 10;
+    public float speed = 5;
+    public float gravity = 20;
+    public static bool canMove;
+
+    private void Start()
+    {
+        playerController = this.GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        if (playerController.isGrounded)
+        {
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= speed;
+            if (Input.GetButton("Jump"))
+            {
+                moveDirection.y = jumpSpeed;
+            }
+
+        }
+        moveDirection.y -= gravity * Time.deltaTime;
+        playerController.Move(moveDirection * Time.deltaTime);
+    }
+}
+
+
+
+
+
+
+
+
+
